a26309c1b828ae9db23131d2f6b63a07e378b586
soud <soud9949ale@gmail.com>
2024-05-12 20:02:47 +0300

    5-12-f1tasck

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Title from './components/Title';
+import Pic from './components/pic';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title/>
+      <h2>our lovely food</h2>
+      <Pic />
+
+      
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+const Title = () => {
+
+    return (
+    <div> 
+       <h1>food&drinks</h1> 
+    </div>)
+}
+
+export default Title
